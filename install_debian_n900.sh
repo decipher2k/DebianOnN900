1fd3d27c373226536fe6ea0f698b76cb6dfd13cb
Dennis M. Heine <dennis.heine@gmx.de>
2019-02-20 01:58:39 +0100

    Update install_debian_n900.sh

--- install_debian_n900.sh
+++ install_debian_n900.sh
@@ -1,12 +1,12 @@
 IMAGENAME=$1
-dd if=./$IMAGENAME.img of=/dev/mmcblk0p1
+dd if=./$IMAGENAME of=/dev/mmcblk0p1
 resize2fs /dev/mmcblk0p1
 apt-get -f install u-boot-flasher
 mkdir /etc/bootmenu.d
 
 cat <<. > /etc/bootmenu.d/Debian.item
 ITEM_NAME="Debian 6 armel on uSD 1"
-ITEM_DEVICE="${INT_CARD}p1"
+ITEM_DEVICE="\${INT_CARD}p1"
 ITEM_FSTYPE="ext3"
 ITEM_KERNEL="/boot/uImage"
 ITEM_INITRD="/boot/uInitrd"
